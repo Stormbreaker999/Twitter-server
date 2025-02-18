5c1ecc3ca95819ef1182608b86c2cdd64302b00b
Avichal <dubeyavichal999@gmail.com>
2025-02-18 09:40:09 +0530

    Changing redis client

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+};
 
  if (process.env.NODE_ENV === 'development') {
    await setupDevPlatform();
--- src/clients/redis/index.ts
+++ src/clients/redis/index.ts
@@ -1,3 +1,4 @@
 import Redis from 'ioredis';
 
-export const redisClient = new Redis("rediss://default:AdSSAAIjcDFlMGQ0ZDE1YzJjMTk0NzU2YTVlYmE5YTRmYzA5YmUzMXAxMA@positive-loon-54418.upstash.io:6379");
+export const redisClient = new Redis("rediss://default:AUBMAAIjcDE5ZTk5YjFhY2RkMTI0NjE3OTY1OGM0ZWJkYzZiZTIxMHAxMA@prime-bulldog-16460.upstash.io:6379");
+
