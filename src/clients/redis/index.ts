7d71eec9da0026def9bada6dc6c247067b5815e5
Avichal <dubeyavichal999@gmail.com>
2025-05-23 20:38:55 +0530

    changing redis client

--- src/clients/redis/index.ts
+++ src/clients/redis/index.ts
@@ -1,4 +1,4 @@
 import Redis from 'ioredis';
 
-export const redisClient = new Redis("rediss://default:AUBMAAIjcDE5ZTk5YjFhY2RkMTI0NjE3OTY1OGM0ZWJkYzZiZTIxMHAxMA@prime-bulldog-16460.upstash.io:6379");
+export const redisClient = new Redis("rediss://default:AVIIAAIjcDExNGUzN2Q3MjZkZGE0ZTg2YTc4YjEwN2ZhZDdmOWU2YnAxMA@in-panda-21000.upstash.io:6379");
 
